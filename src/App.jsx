6a1d3e910d6e0070280542c2a1a5a237d89e54bd
Isti Hoque <115354997+devisti@users.noreply.github.com>
2025-09-12 18:03:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Leaderboard from './pages/LeaderBoard'
 import Header from './components/Header';
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Header />
@@ -17,4 +17,4 @@ function App() {
   )
 }
 
-export default App
+// export default App
